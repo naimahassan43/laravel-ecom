6036670b0148eb201dd1eee3acee620e13b32090
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-05-20 14:15:54 +0100

    Admin Authentication

--- app/Http/Middleware/AdminAuth.php
+++ app/Http/Middleware/AdminAuth.php
@@ -16,6 +16,14 @@ class AdminAuth
      */
     public function handle(Request $request, Closure $next)
     {
+         if( $request->session()->has('Admin_Login')){
+            // $request->session()->put('Admin_Login',true);
+            // $request->session()->put('Admin_Id',$result['0']->id);
+            // return redirect('admin/dashboard');
+        }else{
+            $request->session()->flash('error','Access Denied');
+            return redirect('admin');
+        }
         return $next($request);
     }
 }
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -21,15 +21,6 @@
     <!-- Bootstrap CSS-->
     <link href="{{asset('assets/vendor/bootstrap-4.1/bootstrap.min.css')}}" rel="stylesheet" media="all">
 
-    <!-- Vendor CSS-->
-    {{-- <link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
-    <link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
-    <link href="vendor/wow/animate.css" rel="stylesheet" media="all">
-    <link href="vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
-    <link href="vendor/slick/slick.css" rel="stylesheet" media="all">
-    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
-    <link href="vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all"> --}}
-
     <!-- Main CSS-->
     <link href="{{asset('assets/css/theme.css')}}" rel="stylesheet" media="all">
 
