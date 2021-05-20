2f39dab192cacb5ed39be1124233bceeb26f3eaf
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-05-20 15:35:24 +0100

    Dashboard and Category Page setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,4 +23,5 @@ Route::post('admin/auth',[AdminController::class,'auth'])->name('admin.auth');
 Route::group(['middleware'=>'admin_auth'],function(){
     Route::get('admin/dashboard',[AdminController::class,'dashboard']);
     Route::get('admin/category',[CategoryController::class,'index']);
+    Route::get('admin/manage_category',[CategoryController::class,'manage_category']);
 }); 
